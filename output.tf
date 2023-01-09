a8fa3febc32556c8b951c5be94fb747c5ec95d48
Madhu Kumar S <45975189+smadhukumar@users.noreply.github.com>
2023-01-09 20:01:38 +0530

    connection urls are added

--- output.tf
+++ output.tf
@@ -1,23 +1,22 @@
+###Kafka Server details
 output "LiveLab_Kafka_BootStrap_Server_PublicIP" {
    value = [data.oci_core_vnic.LiveLabWebserver1_VNIC1.public_ip_address]
 }
 
+output "Kafka_Server_Remote_SSH_Connection" {
+  value ="ssh -i ~/.ssh/oci opc@${[data.oci_core_vnic.LiveLabWebserver1_VNIC1.public_ip_address]}"
+  
+}
 #atp
 output "LiveLab_ATP_database_admin_password" {
    value = var.atp_password
 }
 
-output "LiveLab_ATP_databases" {
-  value    = data.oci_database_autonomous_databases.LiveLab_ATP_databases.autonomous_databases
-}
 
 output "parallel_connection_string" {
-  value    = [lookup(oci_database_autonomous_database.LiveLab_ATP_primary_database.connection_strings.0.all_connection_strings, "PARALLEL", "Unavailable")]
+  value    = [lookup(oci_database_autonomous_database.LiveLab_ATP_primary_database.connection_urls, "sql_dev_web_url", "Unavailable")]
+  [lookup(oci_database_autonomous_database.LiveLab_ATP_primary_database.connection_urls[*])]
 }
 
-output "Kafka_Server_Remote_SSH_Connection" {
-  value = formatlist("ssh -i ~/.ssh/oci opc@%s",
-    [data.oci_core_vnic.LiveLabWebserver1_VNIC1.public_ip_address]
-  )
-}
+
 

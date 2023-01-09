86b42b210abc4f840874317f1869ebbfc15dfbe2
Madhu Kumar S <45975189+smadhukumar@users.noreply.github.com>
2023-01-09 20:28:57 +0530

    APEX URL added

--- output.tf
+++ output.tf
@@ -4,8 +4,7 @@ output "LiveLab_Kafka_BootStrap_Server_PublicIP" {
 }
 
 output "Kafka_Server_Remote_SSH_Connection" {
-  value ="ssh -i ~/.ssh/oci opc@${[data.oci_core_vnic.LiveLabWebserver1_VNIC1.public_ip_address]}"
-  
+value ="ssh -i ~/.ssh/oci opc@${data.oci_core_vnic.LiveLabWebserver1_VNIC1.public_ip_address}"
 }
 #atp
 output "LiveLab_ATP_database_admin_password" {
@@ -13,10 +12,8 @@ output "LiveLab_ATP_database_admin_password" {
 }
 
 
-output "parallel_connection_string" {
-  value    = [lookup(oci_database_autonomous_database.LiveLab_ATP_primary_database.connection_urls, "sql_dev_web_url", "Unavailable")]
-  [lookup(oci_database_autonomous_database.LiveLab_ATP_primary_database.connection_urls[*])]
+output "ATP_Connection_URLS" {
+  value    = oci_database_autonomous_database.LiveLab_ATP_primary_database.connection_urls
 }
-
 
 

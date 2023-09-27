629fff5bd71159e133bb88a5b96099b22ef96611
Madhu Kumar S <45975189+smadhukumar@users.noreply.github.com>
2023-09-27 16:02:47 +0530

    Update cloudinitrun.sh
    kafka startup order changed

--- cloudinitrun.sh
+++ cloudinitrun.sh
@@ -73,3 +73,8 @@ sudo chmod +x /tmp/consume.sh
 
 echo "alias consumetopic='/tmp/consume.sh'">>/home/opc/.bash_profile
 echo "alias listtopic='/u01/kafka/bin/kafka-topics.sh --bootstrap-server=localhost:9092 --list'">>/home/opc/.bash_profile 
+
+
+sudo systemctl start zookeeper.service
+sudo systemctl start kafka.service
+
